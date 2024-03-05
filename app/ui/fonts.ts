4c638c5e2a09434353ecf860ad2149d56c9b6e30
infostore <korman21@gmail.com>
2024-03-05 11:50:18 +0900

    ...

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Lusitana, Noto_Sans_KR } from 'next/font/google';
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: "400" });
+export const notoSans = Noto_Sans_KR({subsets: ['latin']});
